989ee224b317e8c2adcb2f49b6eab2f1abb4dfce
jazztickets <jazztickets@gmail.com>
2022-09-10 13:22:06 -0600

    pack data in temp dir

--- scripts/pack.py
+++ scripts/pack.py
@@ -23,6 +23,13 @@ sub_path = arguments.sub_path
 full_path = os.path.join(base_path, sub_path)
 basedirname = os.path.basename(sub_path.strip(os.sep))
 
+# set up temp filenames
+temp_dir = 'temp_' + basedirname + os.path.sep
+header_name = temp_dir + basedirname + '_header'
+body_name = temp_dir + basedirname + '_body'
+final_name = temp_dir + basedirname + '_final'
+os.makedirs(temp_dir, exist_ok=True)
+
 # get list of files
 files = os.listdir(full_path)
 files.sort()
@@ -34,13 +41,13 @@ for filename in files:
 		file_list.append(final_path)
 
 # generate header file
-header_file = open("header", "wb")
+header_file = open(header_name, "wb")
 header_file.write(struct.pack('i', len(file_list)))
 if arguments.debug:
 	print(len(file_list))
 
 # generate data
-data_file = open("body", "wb")
+data_file = open(body_name, "wb")
 for final_path in file_list:
 
 	# load file into memory
@@ -68,11 +75,12 @@ header_file.close()
 data_file.close()
 
 # combine header and body
-with open('final', 'wb') as write_file:
-	for file in ['header', 'body']:
+with open(final_name, 'wb') as write_file:
+	for file in [header_name, body_name]:
 		with open(file, 'rb') as in_file:
 			shutil.copyfileobj(in_file, write_file)
 
 		os.remove(file)
 
-os.rename("final", basedirname + ".bin")
+os.rename(final_name, basedirname + ".bin")
+os.removedirs(temp_dir)

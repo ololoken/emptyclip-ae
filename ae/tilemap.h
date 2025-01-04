cfbb5f9329903530797bf4161da559766cc5a539
jazztickets <jazztickets@gmail.com>
2025-01-04 10:49:01 -0700

    update copyright

--- ae/tilemap.h
+++ ae/tilemap.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (c) 2024 Alan Witkowski
+* Copyright (c) 2025 Alan Witkowski
 *
 * This software is provided 'as-is', without any express or implied
 * warranty. In no event will the authors be held liable for any damages
